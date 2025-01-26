59515e440bcc4c1e30aa44aded8ecb5895b23d1a
Excited669 <142920990+Excited669@users.noreply.github.com>
2025-01-27 00:46:43 +0300

    Added day 6 Task 1 and Task 3

--- src/main/java/day6/Task1.java
+++ src/main/java/day6/Task1.java
@@ -1,7 +1,21 @@
+/*
+В классах Автомобиль и Мотоцикл реализовать два метода:
+info () - выводит в консоль строку “Это автомобиль” (или “Это мотоцикл”),
+yearDifference () - принимает на вход число (год), и возвращает разницу между
+переданным годом и годом выпуска транспортного средства
+ */
 package day6;
 
 public class Task1 {
     public static void main(String[] args) {
 
+        Car car = new Car();
+        Motorbike moto = new Motorbike(2010,"black","Bmw");
+
+        car.info();
+        moto.info();
+        car.setStartYer(2005);
+        System.out.println(car.yearDiference(2010));
+        System.out.println(moto.yearDiference(2011));
     }
 }
--- src/main/java/day6/Task3.java
+++ src/main/java/day6/Task3.java
@@ -3,5 +3,12 @@ package day6;
 public class Task3 {
     public static void main(String[] args) {
 
+        Teacher teacher = new Teacher("Анна Николавна", "Математика");
+        Student student = new Student();
+        teacher.setCourse("Обществознание");
+        student.setName("Коля");
+
+        System.out.println("Преподаватель " + teacher.getName() + " оценил студента с именем " + student.getName() + " по" +
+                " предмету " + teacher.getCourse() + " на оценку " + teacher.evolute(student));
     }
 }
